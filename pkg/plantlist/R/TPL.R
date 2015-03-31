484109d53e991be507ef117b8b96b0069322c6e8
jinlongzhang01 <jinlongzhang01@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-31 03:40:26 +0000

    Rewrite of the TPL function. Rearranged the columns.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plantlist@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plantlist/R/TPL.R
+++ pkg/plantlist/R/TPL.R
@@ -23,29 +23,25 @@ TPL <- function(plant.names = NULL){
         
         return(paste(first, second, sep = ""))
     }
-
-    check.syn.kew <- function(x, db){
-    
-        genera <- get.genus(x) ## Match the word in different cases. 
-        genera <- data.frame(x, genera)
-        res <- merge(x = genera, y = db, by.x = "genera", by.y = "Query", sort = FALSE, all.x = TRUE)
-        res <- res[,c(2,1,3,4)]
-        colnames(res) <- c("YourSpecies", "YourGenus", "KewFamily_Ext", "KewGenus_Ext")
-        return(res)
-    }
     
     kew.path <- system.file("extdata", "KEW_GENERA_EXT.csv", package = "plantlist")
-    kew.base <- read.csv(kew.path,  header = TRUE, stringsAsFactors = FALSE)
+    kew.db <- read.csv(kew.path,  header = TRUE, stringsAsFactors = FALSE)
 
     dd <- system.file("extdata", "TPL_GENERA.csv", package = "plantlist")
-    plantlist.base <- read.csv(dd,  header = TRUE, stringsAsFactors = FALSE)
+    plantlist.db <- read.csv(dd,  header = TRUE, stringsAsFactors = FALSE)
     
-    kew <- check.syn.kew(x = plant.names, db = kew.base)  ## Check against Kew Database
-    res.tpl.family <- merge(x = kew, y = plantlist.base, by.x = "KewGenus_Ext", by.y = "Genus", sort = FALSE, all.x = TRUE)
+    genus <- get.genus(plant.names) ## Match the word in different cases. 
+    genus <- data.frame(YOUR_SPECIES = plant.names, YOUR_GENUS = genus)
+    res0 <- merge(x = genus, y = kew.db, by = "YOUR_GENUS", by.y = "QUERY", sort = FALSE, all.x = TRUE)
+    
+    res1 <- merge(x = res0, y = plantlist.db, by.x = "KEW_GENUS_EXT", by.y = "GENUS", sort = FALSE, all.x = TRUE)
 
     tpl.orders.path <- system.file("extdata", "APG_ORDERS.csv", package = "plantlist")
     tpl.orders <- read.csv(tpl.orders.path, header = TRUE, stringsAsFactors = FALSE)
-    res.tpl.family.orders <- merge(x = res.tpl.family, y = tpl.orders, by = "TPLFamily", sort = FALSE, all.x = TRUE)
-    
-    return(res.tpl.family.orders[,c(3,4,2,5,1,6,7)])
+    res <- merge(x = res1, y = tpl.orders, by = "TPL_FAMILY", sort = FALSE, all.x = TRUE)
+    return(data.frame(YOUR_SPECIES = res$YOUR_SPECIES, YOUR_GENUS = res$YOUR_GENUS, TPL_STATUS = res$TPL_STATUS, TPL_FAMILY = res$TPL_FAMILY, TPL_ORDER = res$TPL_ORDER, APGIII_NUMBER = res$APGIII_NUMBER, KEW_GENUS_EXT = res$KEW_GENUS_EXT, KEW_FAMILY_EXT = res$KEW_FAMILY_EXT, TPL_URL = res$TPL_URL))
 }
+
+
+
+
