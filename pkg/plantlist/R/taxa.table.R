362790f00c386b7828c3203af2178b6a980db46c
jinlongzhang01 (Dr, ) <jinlongzhang01@gmail.com>
2015-03-31 03:40:26 +0000

    Rewrite of the TPL function. Rearranged the columns.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plantlist@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plantlist/R/taxa.table.R
+++ pkg/plantlist/R/taxa.table.R
@@ -1,6 +1,6 @@
 taxa.table <-
 function(x, file = NULL, substitute.sp.white.space = "_"){
-    res <- paste(x$TPLFamily, x$YourGenus, gsub(" ", substitute.sp.white.space, x$YourSpecies), sep = "/")
+    res <- paste(x$TPL_FAMILY, x$YOUR_GENUS, gsub(" ", substitute.sp.white.space, x$YOUR_SPECIES), sep = "/")
     if(!is.null(file)){
         writeLines(res, file)
     }
