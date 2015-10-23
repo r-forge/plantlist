a68fd394e62a4ca1fc691211c38d90289360d721
jinlongzhang01 <jinlongzhang01@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-10-23 09:25:24 +0000

    Upgraded to 0.1.8
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plantlist@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plantlist/R/TPL.R
+++ pkg/plantlist/R/TPL.R
@@ -1,47 +1,49 @@
-TPL <- function(plant.names = NULL){
-
-    options(stringsAsFactors = FALSE ) ## 
-    if(is.null(plant.names)){
+TPL <- function (plant.names = NULL) {    
+    options(stringsAsFactors = FALSE)
+    if (is.null(plant.names)) {
         stop("At least one plant species or genus should be provided.")
     }
-    
-    get.genus <- function(x){  ### Not only accept species , but also the genus name
-        if(is.data.frame(x)){
+    get.genus <- function(x) {
+        Cap <- function(x) {
+            paste(toupper(substring(x, 1, 1)), tolower(substring(x, 
+                2)), sep = "")
+        }
+        if (is.data.frame(x)) {
             x <- as.vector(x)
         }
-        first <- toupper(substr(x, 1, 1 ))  ## change the first alphabet to Upper Case.
+        x <- gsub("^[[:space:]]+|[[:space:]]+$", "", x)
+        x <- Cap(x)
+        first <- substr(x, 1, 1)
         start.point <- 2
-        if(min(regexpr(" ", x)) > 1){
-             end.point <- regexpr(" ", x)
-             second <- substr(x, start.point,  end.point -1)
-        } else {
-             x <- paste(x, " ")
-             end.point <- regexpr(" ", x)
-             second <- substr(x, start.point,  end.point -1)
+        if (min(regexpr(" ", x)) > 1) {
+            end.point <- regexpr(" ", x)
+            second <- substr(x, start.point, end.point - 1)
         }
-        ## the first white space
-        
+        else {
+            x <- paste(x, " ")
+            end.point <- regexpr(" ", x)
+            second <- substr(x, start.point, end.point - 1)
+        }
         return(paste(first, second, sep = ""))
     }
     
-    kew.path <- system.file("extdata", "KEW_GENERA_EXT.csv", package = "plantlist")
-    kew.db <- read.csv(kew.path,  header = TRUE, stringsAsFactors = FALSE)
-
-    dd <- system.file("extdata", "TPL_GENERA.csv", package = "plantlist")
-    plantlist.db <- read.csv(dd,  header = TRUE, stringsAsFactors = FALSE)
+    APGIII_GENERA.path <- system.file("extdata", "APGIII_GENERA.csv", package = "plantlist")
+    APGIII_ORDERS.path <- system.file("extdata", "APGIII_ORDERS.csv", package = "plantlist")
     
-    genus <- get.genus(plant.names) ## Match the word in different cases. 
+    APGIII_GENERA <- read.csv(APGIII_GENERA.path, header = TRUE, stringsAsFactors = FALSE)
+    APGIII_ORDERS <- read.csv(APGIII_ORDERS.path, header = TRUE, stringsAsFactors = FALSE)
+    
+    genus <- get.genus(plant.names)
     genus <- data.frame(YOUR_SPECIES = plant.names, YOUR_GENUS = genus)
-    res0 <- merge(x = genus, y = kew.db, by = "YOUR_GENUS", by.y = "QUERY", sort = FALSE, all.x = TRUE)
     
-    res1 <- merge(x = res0, y = plantlist.db, by.x = "KEW_GENUS_EXT", by.y = "GENUS", sort = FALSE, all.x = TRUE)
+    res1 <- merge(x = genus, y = APGIII_GENERA, by.x = "YOUR_GENUS", 
+        by.y = "GENUS", sort = FALSE, all.x = TRUE)
 
-    tpl.orders.path <- system.file("extdata", "APG_ORDERS.csv", package = "plantlist")
-    tpl.orders <- read.csv(tpl.orders.path, header = TRUE, stringsAsFactors = FALSE)
-    res <- merge(x = res1, y = tpl.orders, by = "TPL_FAMILY", sort = FALSE, all.x = TRUE)
-    return(data.frame(YOUR_SPECIES = res$YOUR_SPECIES, YOUR_GENUS = res$YOUR_GENUS, TPL_STATUS = res$TPL_STATUS, TPL_FAMILY = res$TPL_FAMILY, TPL_ORDER = res$TPL_ORDER, APGIII_NUMBER = res$APGIII_NUMBER, KEW_GENUS_EXT = res$KEW_GENUS_EXT, KEW_FAMILY_EXT = res$KEW_FAMILY_EXT, TPL_URL = res$TPL_URL))
+    res <- merge(x = res1, y = APGIII_ORDERS, by = "APGIII_FAMILY", 
+        sort = FALSE, all.x = TRUE)
+    
+    return(data.frame(YOUR_SEARCH = res$YOUR_SPECIES, POSSIBLE_GENUS = res$YOUR_GENUS, 
+        GENUS_STATUS = res$STATUS, FAMILY = res$APGIII_FAMILY, 
+        ORDER = res$APGIII_ORDER, FAMILY_NUMBER = res$APGIII_NUMBER 
+        ))
 }
-
-
-
-
--- pkg/plantlist/R/taxa.table.R
+++ pkg/plantlist/R/taxa.table.R
@@ -1,6 +1,6 @@
 taxa.table <-
 function(x, file = NULL, substitute.sp.white.space = "_"){
-    res <- paste(x$TPL_FAMILY, x$YOUR_GENUS, gsub(" ", substitute.sp.white.space, x$YOUR_SPECIES), sep = "/")
+    res <- paste(x$FAMILY, x$POSSIBLE_GENUS, gsub(" ", substitute.sp.white.space, x$YOUR_SEARCH), sep = "/")
     if(!is.null(file)){
         writeLines(res, file)
     }
