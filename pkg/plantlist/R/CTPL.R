fe3612f4eda1f0d1d12930fa0d11bc85c3a55bec
jinlongzhang01 <jinlongzhang01@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-20 01:21:05 +0000

    Reversion
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plantlist@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plantlist/R/CTPL.R
+++ pkg/plantlist/R/CTPL.R
@@ -1,12 +1,32 @@
-CTPL <- function(file = NULL){
-    if(is.null(file)){
-       stop("The TXT file containing Chinese Plant Names must be provided!")
-    }
+CTPL <- function(taxa = NULL, taxafile = NULL, outfile = "CTPL_results.csv"){
+
     options(stringsAsFactors = FALSE)
-    cn.path <- system.file("extdata", "CN.csv", package = "plantlist")
-    dat <- read.csv(cn.path, header = TRUE)
-    plant.names <- read.csv(file, header = FALSE)
-    res <- merge(x = plant.names, y = dat, by.x = "V1", by.y = "NAME_CN", sort = FALSE, all.x = TRUE)
-    write.csv(res, "CTPL_results.csv")
-    cat("The file \"CTPL_results.csv\" has been saved to", getwd(), "\n")
+    if(is.null(taxa)&is.null(taxafile)){
+        stop("at least taxa or taxafile should be specified")
+    }
+    if(!is.null(taxa)&!is.null(taxafile)){
+        stop("taxa and taxafile should be not be specified at the same time")
+    }
+    if(is.null(taxa)){
+        taxa <- read.csv(taxafile, header = TRUE, stringsAsFactors = FALSE)
+    } else {
+        taxa <- taxa
+    }
+    
+    taxa <- data.frame(taxa)
+    colnames(taxa) <- "TAXA_NAME"
+    
+    syst <- Sys.info()[['sysname']]
+    if(syst == "Windows"){
+        cn.path <- system.file("extdata", "CN_win.csv", package = "plantlist")
+        dat <- read.csv(cn.path, header = TRUE)
+    } else {
+        cn.path <- system.file("extdata", "CN_linux.csv", package = "plantlist")
+        dat <- read.csv(cn.path, header = TRUE)
+    }
+    
+    res <- merge(x = taxa, y = dat, by.x = "TAXA_NAME", by.y = "NAME", sort = FALSE, all.x = TRUE)
+    write.csv(res, outfile)
+    print(paste("File '", outfile, "' has been saved to", getwd(), sep = ""))
+    return(res)
 }
