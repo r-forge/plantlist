74a54ba0f6549a52f0844887ff5f839d34e85792
jinlongzhang01 (Dr, ) <jinlongzhang01@gmail.com>
2018-11-14 02:06:05 +0000

    Upgraded to 0.5.1, CTPL now works
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/plantlist@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/plantlist/R/CTPL.R
+++ pkg/plantlist/R/CTPL.R
@@ -1,32 +1,23 @@
-CTPL <- function(taxa = NULL, taxafile = NULL, outfile = "CTPL_results.csv"){
-
-    options(stringsAsFactors = FALSE)
-    if(is.null(taxa)&is.null(taxafile)){
-        stop("at least taxa or taxafile should be specified")
-    }
-    if(!is.null(taxa)&!is.null(taxafile)){
-        stop("taxa and taxafile should be not be specified at the same time")
-    }
-    if(is.null(taxa)){
-        taxa <- read.csv(taxafile, header = TRUE, stringsAsFactors = FALSE)
-    } else {
-        taxa <- taxa
-    }
-    
-    taxa <- data.frame(taxa)
-    colnames(taxa) <- "TAXA_NAME"
+CTPL <- function(taxa = NULL, print_as_list = TRUE){
     
     syst <- Sys.info()[['sysname']]
     if(syst == "Windows"){
-        cn.path <- system.file("extdata", "CN_win.csv", package = "plantlist")
-        dat <- read.csv(cn.path, header = TRUE)
-    } else {
-        cn.path <- system.file("extdata", "CN_linux.csv", package = "plantlist")
-        dat <- read.csv(cn.path, header = TRUE)
+        # Ensure that Chinese Characters could be displayed properly.
+        suppressMessages(Sys.setlocale(category = "LC_ALL", locale = "Chinese"))
     }
-    
-    res <- merge(x = taxa, y = dat, by.x = "TAXA_NAME", by.y = "NAME", sort = FALSE, all.x = TRUE)
-    write.csv(res, outfile)
-    print(paste("File '", outfile, "' has been saved to", getwd(), sep = ""))
-    return(res)
+
+    options(stringsAsFactors = FALSE)
+    taxa <- enc2utf8(taxa)
+    taxa <- data.frame(taxa)
+    colnames(taxa) <- "TAXA_NAME"
+
+    cnplants_dat = plantlist::cnplants_dat
+    res <- merge(x = taxa, y = cnplants_dat, by.x = "TAXA_NAME", by.y = "SPECIES_CN", sort = FALSE, all.x = TRUE)
+
+    if(print_as_list){
+        print.listof(res)
+        return(invisible(res))
+    } else {
+        return(res)
+    }
 }
